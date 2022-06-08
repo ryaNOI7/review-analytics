3aed74885d3a515b38b9611f01a1d4a7c7bed6dc
ryaNOI7 <zhongyirniu@gmail.com>
2022-06-08 18:39:39 +0800

    calc avg length of review

--- review.py
+++ review.py
@@ -6,7 +6,9 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0: 
             print(len(cmr))
-print(len(cmr))
-print(cmr[0])
-print('------------')
-print(cmr[1])
+print("檔案讀取完成, 總共有", len(cmr), "筆資料! ")
+
+sum_len = 0 
+for d in cmr:
+    sum_len += len(d)
+print("留言平均長度是: ", sum_len/len(cmr))
